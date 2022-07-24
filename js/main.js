0408bb9ba971e6e7f70dfec939e60a6057a03933
Augusto Rossetti Lorea <augustorossettilorea@gmail.com>
2022-07-24 11:59:45 -0300

    eso

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ function editPlanta(idPlanta) {
 			headers: {
 				'Content-Type': 'application/json',
 			},
-		body: {'id': idPlanta}
+		body: JSON.stringify({'id': idPlanta})
 	  }).then(function(response) {
 		return response.json();
 	  }).then(function(data) {
